c3fffbbe660cb242c92157f2730bfcc7ce042735
morgoevval <92230477+morgoevval@users.noreply.github.com>
2023-04-13 22:37:20 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,25 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as stats
 
+chat_id = 465374385 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+def solution(data) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+    alpha = 0.06
 
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+# Пороговое значение
+    threshold = 300
+
+# Расчет среднего значения и стандартного отклонения
+    mean = sum(data) / len(data)
+    std = stats.tstd(data)
+
+# Расчет t-статистики
+    n = len(data)
+    t_stat = (mean - threshold) / (std / (n**0.5))
+
+# Расчет критического значения
+    crit_val = stats.t.ppf(alpha, df=n-1)
+
+# Принятие решения
+    return t_stat < alpha
